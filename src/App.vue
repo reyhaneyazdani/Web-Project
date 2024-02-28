4147337ab901e469f99be96d17dd19df19afdd13
reyhaneyazdani <reyhanehyx@gmail.com>
2024-02-28 09:53:16 +0330

    frontend

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<script setup>
+import HomePage from './components/HomePage.vue'
+const menu = [{
+  index:1,
+  title : "درباره ی ما"
+},
+{
+  index:2,
+  title : "صلاحیت ها"
+},
+{
+  index:1,
+  title : "پروژه ها"
+},
+{
+  index:1,
+  title : " ارتباط با ما "
+},
+]
+</script>
+
+<template>
+ 
+<div
+      class="h-1/2 lg:h-screen bg-gradient-to-t from-indigo-900 relative overflow-hidden"
+    > 
+      <nav class="w-full fixed top-0 z-10 ">
+        <div class="container mx-auto py-5 flex items-center justify-between">
+          <div class="flex items-center gap-1">
+            <img class="w-32" src="/logo png.png" alt="" />
+          </div>
+        </div>
+      </nav>
+      <div class="fixed right-16 bottom-2 z-10 border-dashed border-2 border-[#9747FF] px-3 py-2 rounded-lg">
+        <div v-for="(item, index) in menu" :key="index" class="py-1 px-8 text-center bg-[#D9D9D9]/50 text-white rounded-full my-4">
+          <h1 class="tracking-wide text-lg">{{item.title }}</h1>
+        </div>
+        <p class="text-xs text-white pb-1 font-light">مهندسین مشاور فربوم شهر پارس</p>
+  
+      </div>
+     
+      <img
+        class="object-cover w-full absolute "
+        src="/Page 24-2.png"
+        alt=""
+      />
+   
+    <!-- <img
+      class="object-cover  py-20 w-full h-screen  relative"
+      src="/iranian-building-85236987-10.jpg"
+      alt=""
+    /> -->
+  </div>
+    <div class="mb-16">
+  <HomePage  />
+</div>  
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,77 @@
+<script setup>
+const details = [
+	{
+		title: "Organize your team the way you want.",
+		content:
+			"One who is injured ought not to return the injury, for on no account can it be right to do an injustice; and it is not right to return an injury, or to do evil to any man, however much we have suffered from him.",
+		image: {
+			src: "/Page 24-1.png",
+			width: 650,
+			height: 550,
+		},
+	
+	},
+  {
+		title: "Organize your team the way you want.",
+		content:
+			"One who is injured ought not to return the injury, for on no account can it be right to do an injustice; and it is not right to return an injury, or to do evil to any man, however much we have suffered from him.",
+		image: {
+			src: "/Page 16.png",
+			width: 650,
+			height: 550,
+		},
+	
+	},
+  {
+		title: "Organize your team the way you want.",
+		content:
+			"One who is injured ought not to return the injury, for on no account can it be right to do an injustice; and it is not right to return an injury, or to do evil to any man, however much we have suffered from him.",
+		image: {
+			src: "/Page 18.png",
+			width: 650,
+			height: 550,
+		},
+	
+	},
+	{
+		title: "Organize your team the way you want.",
+		content:
+			"One who is injured ought not to return the injury, for on no account can it be right to do an injustice; and it is not right to return an injury, or to do evil to any man, however much we have suffered from him.",
+		image: {
+			src: "/Page 20.png",
+			height: 550,
+		},
+	
+	},
+]
+
+</script>
+  <template>
+  
+  <div v-for="(item, index) in details" :key="index" class="group bg-[#1E1E1E] py-12 first:pt-0 last:pb-0 even:bg-light lg:py-20 md:py-16">
+  <div class="container mx-auto">
+	<div className="block items-center group-even:flex-row-reverse md:space-y-10 lg:m-0 lg:flex lg:space-y-16 xl:mx-10">
+              <div className="mx-16 mb-10 text-start lg:m-0 lg:w-full xl:mx-10">
+                
+                <div className="my-6 text-xl text-white font-semibold tracking-wider md:mb-6 md:text-2xl">
+                  {{item.title}}
+                </div>
+                <div className="tracking-wider text-gray-300 md:text-sm">{{item.content}}</div>
+              </div>
+              <img
+                className="min-w-xs mb-4 mx-10 max-w-md lg:max-w-2xl lg:w-full"
+                :src=item.image.src
+                :width=item.image.width
+                :height=item.image.height
+                alt=""
+              />
+            </div>
+   		 </div>
+  </div>
+
+
+</template>
+
+<style scoped>
+
+</style>
